7d9c1db7e904ea1e16442f6e9d7e2ce6c55c9b2a
Anna Shnir <shnir.anna@gmail.com>
2015-04-12 17:12:42 -0400

    started working on specs within readme.md, server and some landing pages for the blog

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Blog
+User will be able to make posts in the blog
+User wil be able to edit the existing posts
+and delete them.
+
+The information entered by the user will be stored in a database.
---
+++ server.js
@@ -0,0 +1,40 @@
+//app requirements
+//express
+var express = require('express')
+var app = express();
+
+//templating stuff
+var ejs = require("ejs")
+app.set("view engine", "ejs")
+//body parser
+var bodyParser = require('body-parser')
+//tell app which method to use when parsing
+app.use(bodyParser.urlencoded({extended: false}))
+
+//method override setup
+var methodOverride = require('method-Override')
+//tell app which override method to use
+app.use(methodOverride('_method'))
+
+//allow sqlite3
+var sqlite3 = require('sqlite3').verbose();
+//set database
+// var db = new sqlite3.Database('./db/movies.db');
+//request
+var request = require('request')
+
+//routes
+
+//idiot test
+app.get('/', function(req, res){
+	res.redirect('/index')
+});
+app.get("/index", function(req, res) {
+	res.render("index.ejs", {posts: posts});
+	console.log(req.body);
+	console.log(res.body);
+})
+
+
+app.listen('3000')
+console.log("Listening on port 3000")
